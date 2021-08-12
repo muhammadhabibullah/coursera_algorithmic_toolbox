bd5aeea657c570e9591ffaaba19e6c014e1b9a8a
Muhammad Habibullah <muhammadhabibullah.id@gmail.com>
2021-08-12 20:56:46 +0700

    improve quick sort

--- week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <tuple>
 
-using std::vector;
+using std::get;
 using std::swap;
+using std::tuple;
+using std::vector;
 
-int partition2(vector<int> &a, int l, int r) {
+int partition2(vector<int> &a, int l, int r)
+{
   int x = a[l];
   int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
+  for (int i = l + 1; i <= r; i++)
+  {
+    if (a[i] <= x)
+    {
       j++;
       swap(a[i], a[j]);
     }
@@ -18,28 +24,64 @@ int partition2(vector<int> &a, int l, int r) {
   return j;
 }
 
-void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
+tuple<int, int> partition3(vector<int> &a, int left, int right)
+{
+  int x = a[left];
+  int m1 = left;
+  int m2 = right;
+  int i = left;
+
+  while (i <= m2)
+  {
+    if (a[i] < x)
+    {
+      swap(a[i], a[m1]);
+      m1++;
+      i++;
+    }
+
+    else if (a[i] > x)
+    {
+      swap(a[i], a[m2]);
+      m2--;
+    }
+
+    else
+    {
+      i++;
+    }
+  }
+
+  return {m1, m2};
+}
+
+void randomized_quick_sort(vector<int> &a, int l, int r)
+{
+  if (l >= r)
+  {
     return;
   }
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  auto middles = partition3(a, l, r);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, get<0>(middles) - 1);
+  randomized_quick_sort(a, get<1>(middles) + 1, r);
 }
 
-int main() {
+int main()
+{
   int n;
   std::cin >> n;
   vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
+  for (size_t i = 0; i < a.size(); ++i)
+  {
     std::cin >> a[i];
   }
   randomized_quick_sort(a, 0, a.size() - 1);
-  for (size_t i = 0; i < a.size(); ++i) {
+  for (size_t i = 0; i < a.size(); ++i)
+  {
     std::cout << a[i] << ' ';
   }
 }
