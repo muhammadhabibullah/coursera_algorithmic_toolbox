c9adc2a09762d96fe0a5bc47e3d726a03ffcd5a7
Muhammad Habibullah <muhammadhabibullah.id@gmail.com>
2021-08-12 19:07:10 +0700

    binary search

--- week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -4,33 +4,56 @@
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+int binary_search(const vector<int> &a, int first, int last, int x)
+{
+  if (last < first)
+  {
+    return -1;
+  }
+
+  int mid = first + (last - first) / 2;
+
+  if (x == a[mid])
+  {
+    return mid;
+  }
+
+  else if (x < a[mid])
+  {
+    return binary_search(a, first, (mid - 1), x);
+  }
+
+  return binary_search(a, (mid + 1), last, x);
 }
 
-int linear_search(const vector<int> &a, int x) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
+int linear_search(const vector<int> &a, int x)
+{
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+    if (a[i] == x)
+      return i;
   }
   return -1;
 }
 
-int main() {
+int main()
+{
   int n;
   std::cin >> n;
   vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
+  for (size_t i = 0; i < a.size(); i++)
+  {
     std::cin >> a[i];
   }
   int m;
   std::cin >> m;
   vector<int> b(m);
-  for (int i = 0; i < m; ++i) {
+  for (int i = 0; i < m; ++i)
+  {
     std::cin >> b[i];
   }
-  for (int i = 0; i < m; ++i) {
-    //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+  for (int i = 0; i < m; ++i)
+  {
+    std::cout << binary_search(a, 0, (int)a.size() - 1, b[i]) << ' ';
   }
 }
