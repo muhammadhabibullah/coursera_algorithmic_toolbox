4fdf12456e1f86644ec84a2894b91b397cda8d31
Muhammad Habibullah <muhammadhabibullah.id@gmail.com>
2021-08-18 20:56:16 +0700

    change dp

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <map>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+using std::map;
+using std::pair;
+
+int get_change(int money)
+{
+  const int biggestNum = 1000;
+  int coins[3] = {1, 3, 4};
+
+  map<int, int> min_num_coins;
+  min_num_coins[0] = 0;
+  for (int m = 1; m <= money; m++)
+  {
+    min_num_coins[m] = biggestNum;
+
+    for (int i = 0; i < 3; i++)
+    {
+      if (m >= coins[i])
+      {
+        int numCoins = min_num_coins[m - coins[i]] + 1;
+        if (numCoins < min_num_coins[m])
+        {
+          min_num_coins[m] = numCoins;
+        }
+      }
+    }
+  }
+
+  return min_num_coins[money];
 }
 
-int main() {
+int main()
+{
   int m;
   std::cin >> m;
   std::cout << get_change(m) << '\n';
