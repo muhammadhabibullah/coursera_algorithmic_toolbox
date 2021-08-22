db07dde3b5460d75c9e91ae099f10add65f76d8e
Muhammad Habibullah <muhammadhabibullah.id@gmail.com>
2021-08-22 07:46:00 +0700

    primitive calculator

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -4,28 +4,54 @@
 
 using std::vector;
 
-vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
+vector<int> optimal_sequence(int n)
+{
+  vector<int> min_steps(n + 1, 0);
+  vector<int> predecessor(n + 1);
+  min_steps[0] = 1;
+
+  for (int i = 2; i <= n; ++i)
+  {
+    min_steps[i] = min_steps[i - 1] + 1;
+    predecessor[i] = i - 1;
+
+    if (i % 2 == 0)
+    {
+      if (min_steps[i / 2] < min_steps[i])
+      {
+        min_steps[i] = min_steps[i / 2] + 1;
+        predecessor[i] = i / 2;
+      }
+    }
+    if (i % 3 == 0)
+    {
+      if (min_steps[i / 3] < min_steps[i])
+      {
+        min_steps[i] = min_steps[i / 3] + 1;
+        predecessor[i] = i / 3;
+      }
     }
   }
+
+  vector<int> sequence;
+
+  for (int i = n; i != 0; i = predecessor[i])
+  {
+    sequence.push_back(i);
+  }
+
   reverse(sequence.begin(), sequence.end());
   return sequence;
 }
 
-int main() {
+int main()
+{
   int n;
   std::cin >> n;
   vector<int> sequence = optimal_sequence(n);
   std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
+  for (size_t i = 0; i < sequence.size(); ++i)
+  {
     std::cout << sequence[i] << " ";
   }
 }
